598cb46a10de212b593a781ebdaa73327f5eaac9
Etonam KOUTODZO <mamx1404@gmail.com>
2026-02-16 00:07:06 +0000

    Add HTTPS scheme enforcement for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
